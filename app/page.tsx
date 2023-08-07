8d16631390593e0bc9af62e046a3fa721af01ed3
Rex739 <ojemehprogress@gmail.com>
2023-08-07 23:33:43 +0100

    initailized homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import Button from "@/components/ui/button"
 import { FC } from "react"
 
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
-  return <div className="text-blue-400">pastor </div>
+  return (
+    <div className="w-11/12 mx-auto py-5">
+      
+      <Button size={"lg"}>hello </Button>
+    </div>
+  )
 }
 
 export default page
