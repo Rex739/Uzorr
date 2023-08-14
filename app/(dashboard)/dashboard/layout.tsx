f474b1a1d3bcc829d466bde2231458b623f217b4
Rex739 <ojemehprogress@gmail.com>
2023-08-14 14:05:59 +0100

    code update on dashboard sidebar layout

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -4,6 +4,9 @@ import { FC, ReactNode } from "react"
 import { notFound } from "next/navigation"
 import Link from "next/link"
 import { Icon, Icons } from "@/components/icons"
+import Image from "next/image"
+import SignOutButton from "@/components/signOutButton" 
+import FriendRequestSidebarOptions from "@/components/friendRequestSidebarOptions"
 
 interface LayoutProps {
   children: ReactNode
@@ -65,7 +68,32 @@ const Layout: FC<LayoutProps> = async ({ children }) => {
                 })}
               </ul>
             </li>
-            <li className="mx-6 mt-auto"> </li>
+            <li>
+
+            <FriendRequestSidebarOptions />
+            </li>               
+            <li className="-mx-6 mt-auto flex items-center">
+              <div className="flex flex-1 items-center gap-x-4 px-6 py-3 text-sm font-semibold  leading-6 text-gray-900">
+                <div className="relative h-8 w-8 bg-gray-50">
+                  <Image
+                    fill
+                    referrerPolicy="no-referrer"
+                    className="rounded-full "
+                    src={session.user.image || ""}
+                    alt="Your profile picture"
+                  />
+                </div>
+
+                <span className="sr-only">Your profile</span>
+                <div className="flex flex-col">
+                  <span aria-hidden="true">{session.user.name}</span>
+                  <span className="text-xs text-zinc-400" area-hidden="true">
+                    {session.user.email}
+                  </span>
+                </div>
+              </div>
+              <SignOutButton className="h-full aspect-square" />
+            </li>
           </ul>
         </nav>
       </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import Providers from "@/components/ui/providers"
+import Providers from "@/components/providers"
 import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
