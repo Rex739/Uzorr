c0643e08c2646100ea3d350b74b013f5bfeda1ea
Rex739 <ojemehprogress@gmail.com>
2023-08-14 21:56:27 +0100

    fixed type error in Link href

--- components/friendRequestSidebarOptions.tsx
+++ components/friendRequestSidebarOptions.tsx
@@ -1,19 +1,35 @@
-import { Link, User } from "lucide-react"
-import { FC } from "react"
+"use client"
 
-interface FriendRequestSidebarOptionsProps {}
+import { User } from "lucide-react"
+import { FC, useState } from "react"
+import Link from "next/link"
+import { number } from "zod"
+interface FriendRequestSidebarOptionsProps {
+  sessionId: string
+  initialUnseenRequestCount: number
+}
 
-const FriendRequestSidebarOptions: FC<
-  FriendRequestSidebarOptionsProps
-> = ({}) => {
+const FriendRequestSidebarOptions: FC<FriendRequestSidebarOptionsProps> = ({
+  sessionId,
+  initialUnseenRequestCount,
+}) => {
+  const [unseenRequestCount, setUnseenRequestCount] = useState<number>(
+    initialUnseenRequestCount
+  )
   return (
     <Link
-      href="/dashboard/request"
-      className="text-gray-600 hover:text-indigo-600 hover:bg-gray-50 group flex items-center gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+      href="/dashboard/requests"
+      className="text-gray-600 hover:text-indigo-600 hover:bg-gray-50 group flex rounded-md gap-3 leading-6 text-sm p-2 font-semibold"
     >
-      <div className="text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white">
-        <User className="h-4 w-4"/>
-      </div>
+      <span className="text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-meduim bg-white">
+        <User className="h-4 w-4" />
+      </span>
+      <span className="truncate">Friend requests</span>
+      {unseenRequestCount > 0 ? (
+        <div className="rounded-full w-5 h-5 text-xs flex justify-center items-center text-white bg-indigo-600">
+          {unseenRequestCount}
+        </div>
+      ) : null}
     </Link>
   )
 }
