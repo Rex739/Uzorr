8236a1809f8002886948f9303d1d194f11e99d96
Rex739 <ojemehprogress@gmail.com>
2023-08-26 15:45:03 +0100

    created middleware to app protection, handles all authourizations, pages you can access while authoirzed and so forth

---
+++ middleware.ts
@@ -0,0 +1,44 @@
+import { getToken } from "next-auth/jwt"
+import { withAuth } from "next-auth/middleware"
+import { NextResponse } from "next/server"
+
+export default withAuth(
+  async function middleware(req) {
+    const pathName = req.nextUrl.pathname
+
+    //manage route protection
+    const isAuth = await getToken({ req })
+    const isLoginPage = pathName.startsWith("/login")
+    const sensitiveRoutes = ["/dashboard"]
+    const isAccesingSensitiveRoutes = sensitiveRoutes.some((route) =>
+      pathName.startsWith(route)
+    )
+
+    if (isLoginPage) {
+      if (isAuth) {
+        return NextResponse.redirect(new URL("/dashboard", req.url))
+      }
+
+      return NextResponse.next()
+    }
+
+    if (!isAuth && isAccesingSensitiveRoutes) {
+      return NextResponse.redirect(new URL("/auth/login", req.url))
+    }
+    if (pathName === "/") {
+      return NextResponse.redirect(new URL("/auth/login", req.url))
+    }
+  },
+
+  {
+    callbacks: {
+      async authorized() {
+        return true
+      },
+    },
+  }
+)
+
+export const config = {
+  matcher: ["/", "/login", "/dashboard/:path*"],
+}
