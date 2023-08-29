18b146aff2c0f7724df9a259afeee5b607100314
Rex739 <ojemehprogress@gmail.com>
2023-08-29 12:22:21 +0100

    created function that sorts paramIDs and seperates it whith double dash sign

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,12 @@
-import clsx from "clsx";
-import { ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import clsx from "clsx"
+import { ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs))
 }
 
+export function chatHrefConstructor(id1: string, id2: string) {
+  const sortedId = [id1, id2].sort()
+  return `${sortedId[0]}--${sortedId[1]}`
+}
